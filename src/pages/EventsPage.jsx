c9b42bdcc882097e13678e1ec174e39f40421098
Mirza-Hamza-Tariq <ht9480488@gmail.com>
2026-01-12 11:47:00 +0500

    build-changes-final

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,5 +1,5 @@
 // src/pages/EventsPage.jsx
-import React, { useState } from "react";
+import React from "react";
 // import CartModal from "../components/EventCart";
 import "../pages/EventPage.css"; // Your existing CSS
 // import eventtimg from "../assets/Event image.jpg";
